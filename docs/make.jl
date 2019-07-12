863782acc463cc61e3429a9ce230adfc776d94d7
Takafumi Arakaki <aka.tkf@gmail.com>
2019-07-11 17:26:47 -0700

    Setup CI

--- docs/make.jl
+++ docs/make.jl
@@ -9,7 +9,6 @@ makedocs(;
     repo="https://github.com/tkf/NDReducibles.jl/blob/{commit}{path}#L{line}",
     sitename="NDReducibles.jl",
     authors="Takafumi Arakaki <aka.tkf@gmail.com>",
-    assets=String[],
 )
 
 deploydocs(;
