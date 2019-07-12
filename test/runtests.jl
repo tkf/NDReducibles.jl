24f819ebc48563e3bdc371167d9c649c4ec2689a
Takafumi Arakaki <aka.tkf@gmail.com>
2019-07-11 17:44:18 -0700

    Ignore first failure in test_inference.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 module TestNdreducible
 using Test
 
+@info "Running test_inference.jl for the first time."
+try
+    include("test_inference.jl")
+catch
+    @info "Got an error as expected..."
+end
+
+@info "Running test_inference.jl for the second time. This should work..."
+include("test_inference.jl")
+
 @testset "$file" for file in sort([file for file in readdir(@__DIR__) if
                                    match(r"^test_.*\.jl$", file) !== nothing])
     include(file)
