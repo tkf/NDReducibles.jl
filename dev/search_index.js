cc342ae5909176003bc6d153c4bddb339533da05
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-07-12 00:36:10 +0000

    build based on 863782a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#NDReducibles.jl-1","page":"Home","title":"NDReducibles.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [NDReducibles]","category":"page"},{"location":"#NDReducibles.plan-Tuple{Vararg{NDReducibles.AccessPattern,N} where N}","page":"Home","title":"NDReducibles.plan","text":"plan(args::AccessPattern...) :: AccessPlan\n\nTry to find an index that is categorized as \"fast\" for all args with such index.\n\n\n\n\n\n","category":"method"}]
+}
